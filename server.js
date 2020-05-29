254a48d8019a02dcb6edab806489724223fa27f1
LamaIhab <lamloma19@hotmail.com>
2020-05-29 18:27:27 +0200

    connected the app to mongodb (database)

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+
 // create the app
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 5000
-app.listen(port,()=>console.log(`server running on ${port}`))
+// connect to mongoDB
+const db = require("./config/keys.js").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(error => console.log(error));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server running on ${port}`));
